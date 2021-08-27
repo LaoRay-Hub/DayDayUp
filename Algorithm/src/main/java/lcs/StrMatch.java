682cdcf7cd3dc15af83afd640b0eb0e9a17c2036
LaoRay <zhengyuanlei@gmail.com>
2021-08-27 20:02:58 +0800

    字符串映射封装

--- Algorithm/src/main/java/lcs/StrMatch.java
+++ Algorithm/src/main/java/lcs/StrMatch.java
@@ -1,8 +1,8 @@
 package lcs;
 
-import javax.xml.transform.Source;
+import com.alibaba.fastjson.JSONObject;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static java.lang.Math.max;
@@ -10,34 +10,39 @@ import static java.lang.Math.max;
 public class StrMatch {
     public static void main(String[] args) {
 
-        String a="abc";
-        String b="ab";
-
          List<String> aList = new ArrayList<>();
-         aList.add("col1");
          aList.add("col2");
+         aList.add("col1");
          aList.add("col3");
+         aList.add("col4");
 
         List<String> bList = new ArrayList<>();
         bList.add("col1");
-        bList.add("col");
         bList.add("col3");
+        bList.add("col");
+        bList.add("col2");
 
+        System.out.println(strMatch(aList, bList));
+
+    }
+
+    public static JSONObject strMatch (List<String> aList, List<String> bList){
+        int aSize = aList.size();
+        int bSize = bList.size();
+        String [] matched=new String[aSize];
         float[][] scores  = calcScores(aList, bList);
-        for (int i = 0; i <3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(scores[i][j]+",");
 
+        for (int i = 0; i <aSize; i++) {
+            for (int j = 0; j < bSize; j++) {
+                System.out.print(scores[i][j]+",");
             }
             System.out.println();
         }
-//        List<String> matched  = new ArrayList<>(5);
 
-        String [] matched=new String[3];
+        //找到scores最大的位置
         for (int i = 0; i < aList.size(); i++) {
             int row=0,col=0;
             double maxValue=scores[0][0];
-            int [][] re=new int [scores.length][scores.length];
             for (int j = 0; j < scores.length; j++) {
                 for (int k = 0; k < scores[0].length; k++) {
                     if(scores[j][k]>maxValue){
@@ -47,58 +52,42 @@ public class StrMatch {
                     }
                 }
             }
-            scores[row][0]=-1f;
-            scores[row][1]=-1f;
-            scores[row][2]=-1f;
-            scores[0][col]=-1f;
-            scores[1][col]=-1f;
-            scores[2][col]=-1f;
 
-            System.out.println("============");
-            for (int n = 0; n <3; n++) {
-                for (int j = 0; j < 3; j++) {
-                    System.out.print(scores[n][j]+",");
-
-                }
-                System.out.println();
-            }
+            //取出匹配分最高的数，存入数组
             matched[row]=bList.get(col);
+            //将评分表行列分数置为-1
+            for (int j = 0; j < scores.length; j++) {
+                scores[row][j]=-1f;
+                scores[j][col]=-1f;
+            }
         }
 
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("col1",aList);
+        jsonObject.put("col2",matched);
 
-
-
-        System.out.println(aList);
-        System.out.println(bList);
-        System.out.println(Arrays.toString(matched));
-
+        return jsonObject;
     }
-
 
     public static float [][] calcScores(List<String> a, List<String> b){
 
-        int asize = a.size();
+        int aSize = a.size();
+        int bSize = b.size();
+        float [][] scores=new float[aSize][bSize];
 
-        float [][] scores=new float[3][3];
-
-        for (int i = 0; i <3; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i <aSize; i++) {
+            for (int j = 0; j < bSize; j++) {
                 int lcs=lcs(a.get(i),b.get(j));
-
                 scores[i][j]=lcs+(float)lcs/max(a.get(i).length(),b.get(j).length());
-
             }
         }
-
         return scores;
-
     }
-
 
     public static int lcs(String str1, String str2) {
         int len1 = str1.length();
         int len2 = str2.length();
-        int c[][] = new int[len1+1][len2+1];
+        int[][] c = new int[len1+1][len2+1];
         for (int i = 0; i <= len1; i++) {
             for( int j = 0; j <= len2; j++) {
                 if(i == 0 || j == 0) {
